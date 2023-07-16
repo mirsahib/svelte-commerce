249950a46d6ee042b2f2f13a2120ebe8a4b6625a
Mir Sahib <mirsahib24@gmail.com>
2023-07-16 11:35:36 +0600

    :hammer: Add catagory route

---
+++ src/routes/category/[catagoryId]/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { page } from "$app/stores";
+  const { catagoryId } = $page.params;
+  import { products } from "$lib/products";
+  import Card from "$components/Card.svelte";
+
+  const filterProducts = products.filter(product=>(product.category===catagoryId))
+</script>
+
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap -m-4">
+      {#each filterProducts as product}
+        <Card {product} />
+      {/each}
+    </div>
+  </div>
+</section>
