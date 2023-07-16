87063902c259d2da92c062cd8b6c35e3a96ccc11
Mir Sahib <mirsahib24@gmail.com>
2023-07-16 19:43:39 +0600

    :hammer: Delete product from cart

---
+++ src/components/ProductRow.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { Trash2 } from "lucide-svelte";
+  import type {ProductType} from "$lib/products"
+  import cart from "$store/cart";
+  export let product:ProductType
+
+  const handleRemoveProduct = (product:ProductType) => {
+    console.log("🚀 ~ file: ProductRow.svelte:8 ~ handleRemoveProduct ~ product:", product)
+    cart.removeFromCart(product)
+  }
+
+</script>
+
+<!-- Table rows here -->
+<tr>
+  <td class="py-2 px-4 border">
+    <!-- Added border class here -->
+    <img src={product.img} alt="Product" class="h-10 w-10 rounded-full" />
+  </td>
+  <td class="py-2 px-4 border">
+    <!-- Added border class here -->
+    {product.title}
+  </td>
+  <td class="py-2 px-4 border">
+    <!-- Added border class here -->
+    ${product.price}
+  </td>
+  <td class="py-2 px-4 border">
+    <!-- Added border class here -->
+    2
+  </td>
+  <td class="py-2 px-4 border">
+    <!-- Added border class here -->
+    $19.98
+  </td>
+  <td class="py-2 px-4 border">
+    <!-- Added border class here -->
+    <button on:click={()=>handleRemoveProduct(product)}>
+      <Trash2 />
+    </button>
+  </td>
+</tr>
